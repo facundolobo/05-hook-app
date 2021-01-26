eb76d4893e8ba4cc9c392b42fd295fa17309b416
Facundo Lobo <facundolobo2@gmail.com>
2021-01-26 06:26:50 -0300

    10. useFetch - CustomHook

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import { HooksApp } from './HooksApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+//import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
-
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
 ReactDOM.render(
   
-    < FormWithCustomHook />,
+    < MultipleCustomHooks />,
   
   document.getElementById('root')
 );
