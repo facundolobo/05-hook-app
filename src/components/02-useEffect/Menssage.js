da6c524f1daedc3a0cfa4a831fb5ad0eb67c433e
Facundo Lobo <facundolobo2@gmail.com>
2021-01-26 02:33:59 -0300

    8. useEffect - Precauciones

--- src/components/02-useEffect/Menssage.js
+++ src/components/02-useEffect/Menssage.js
@@ -1,22 +1,40 @@
-
-
-
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 
 export const Menssage = () => {
 
-    useEffect(() => {
+//------------------------------------------
 
-        console.log('componente montado');
+const [coords, setCoords] = useState({x:0 , y: 0}) //instancia de variables
+const {x, y} = coords;    
 
-        return () => {
-            console.log('componente desmontado');
+    
+    //------------------------------------------    
+        useEffect(() => { //solo se activa al escribir 123 en nombre.
+
+            //funcion para devolver las coordenadas x e y 
+            const mouseMove = (e) => {
+                
+                const coords = {x: e.x , y:e.y} //obtiene las coordenadas del mouse
+                setCoords(coords) //actualiza las coordenadas del mouse 
+        
+            }
+            
+            window.addEventListener('mousemove' , mouseMove ) //agrega un escuchador al movimiento de mouse 
+
+        return () => { //cuando ya se desmonta el efecto  
+            
+            window.removeEventListener('mousemove', mouseMove ) //remueve el escuchador
         }
-    }, [])
+        }, []) 
+    //------------------------------------------------
+
 
     return (
         <div>
             <h3>Eres Genial!</h3>
+            <p>
+                x: {x} , y{y} 
+            </p>
         </div>
     )
 }
