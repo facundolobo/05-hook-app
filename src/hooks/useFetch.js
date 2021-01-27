84185e540d3c8afb933873860674a644db7abc74
Facundo Lobo <facundolobo2@gmail.com>
2021-01-27 09:43:47 -0300

    13. useRef - Caso de uso real

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,9 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const useFetch = (url) => {
     
+    const isMounted = useRef(true); //significa esta montado, mantiene la referencia cuando el componente esta montado
     const [state, setState] = useState({data:null , loading: true,error:null});
         
+        useEffect(() => {
+            
+            return () => {
+                isMounted.current = false; //lo cambia a false cuando se desmonta 
+            }
+        }, [])
+    
+    
         useEffect(() => {
             
             setState({data:null , loading: true,error:null}); //para que aparesca el "loading" antes de cargar la siguiente cita
@@ -11,12 +20,24 @@ export const useFetch = (url) => {
             fetch( url )
                 .then(resp => resp.json())
                 .then(data => {
-                
-                    setState({
-                    loading: false,
-                    error: null,
-                    data
-                    })
+                    
+                    
+                    //funcion que espera para ejecutar
+                    //setTimeout(() => {
+                        if(isMounted.current){ //si es true, significa q esta mantado 
+                        
+                            setState({
+                            loading: false,
+                            error: null,
+                            data
+                            })
+                            console.log("setState SI se llamo");
+                        }
+                        //  else{
+                        //     console.log("setState NO se llamo");
+                        // }
+                        
+                    //}, 4000); //espera 4 segundo
             })
         }, [url]) 
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-
 import ReactDOM from 'react-dom';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 
+//import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import { HooksApp } from './HooksApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 //import { SimpleForm } from './components/02-useEffect/SimpleForm';
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+//import { FocusScreen } from './components/04-useRef/FocusScreen';
 
-import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
 ReactDOM.render(
   
-    < FocusScreen />,
+    < RealExampleRef />,
   
   document.getElementById('root')
 );
