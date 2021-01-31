fd07c0a2be329e4f84c843446bc7ade8ed1e1573
Facundo Lobo <facundolobo2@gmail.com>
2021-01-31 03:28:04 -0300

    6. Creando el cascaron de la lista de TODOs

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -16,14 +16,52 @@ export const TodoApp = () => {
     console.log(todos);
 
     return (
-        <div>
-           <h1>TodoApp</h1> 
+        <div className="row"> 
+           <h1>TodoApp ({todos.length})</h1> 
            <hr/>
-           <ul>
-               <li>hola</li>
-               <li>mundo</li>
-               <li>hola de nuevo</li>
-           </ul>
+            {/*primera columna */}
+            <div className="col-7">
+                <ul className="list-group list-group-flush"  > 
+                {
+                    todos.map( (todo,i )=> (
+                        <li 
+                            key={todo.id}
+                            className=" list-group-item"
+                        > 
+                            <p className="text-center complete">{i +1}. {todo.desc} </p>
+                            <button className="btn btn-danger">
+                                Borrar</button> 
+
+                        </li>
+                    ))
+                }
+                </ul>
+
+            </div>
+            {/*segunda columna */}
+            <div className="col-4">
+                <h4>
+                    agregar TODO
+                </h4>
+                <hr/>
+
+                {/* formulario apra agregar una tarea */}
+                <form>
+                    <input 
+                        type="text"
+                        name="description"
+                        className="form-control"
+                        placeholder="aprender..."
+                        autoComplete="off"
+                    />
+                    <button 
+                    className="btn btn-outline-primary mt-1 btn-block"
+                    >
+                        Agregar
+                    </button>
+                </form>
+                {/*------------------------------------ */}
+            </div>
 
         </div>
     )
